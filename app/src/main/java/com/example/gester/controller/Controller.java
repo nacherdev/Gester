f8ad6bf7347e4d1d163f5c66d385c16d90066f3f
Julian Barbero <julian.barbero.perea@gmail.com>
2026-05-19 20:15:35 +0200

    Controller to connect bbdd

--- app/src/main/java/com/example/gester/controller/Controller.java
+++ app/src/main/java/com/example/gester/controller/Controller.java
@@ -1,5 +1,24 @@
 package com.example.gester.controller;
 
+import com.example.gester.dao.Dao;
+import com.example.gester.dao.models.Cita;
+
+import java.util.ArrayList;
+
 public class Controller {
+    private Dao d = null;
+        public boolean conectarBBDD(String nombrebd, String usuario, String contrasena){
+            d = new Dao(nombrebd,usuario,contrasena);
+            if(d.conectar() == false){
+                return false;
+            }else {
+                return true;
+            }
+        }
+
+        public ArrayList<Cita> getCitas(){
+            return d.todasLasCitas();
+        }
+
 
 }
